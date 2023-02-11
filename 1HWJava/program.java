4b83636b82782495f7164664d7df7e277a746599
KoksharovSA_LT <koksharovsa@gmail.com>
2023-02-11 20:14:05 +0700

    Add 1 home work

---
+++ 1HWJava/program.java
@@ -0,0 +1,31 @@
+//Написать программу вычисления n-ого треугольного числа
+
+public class program {
+    public static void main(String[] args) { 
+        System.out.println(triangularNumber(7));
+        printChristmasTree(7);
+    }
+
+    public static int triangularNumber(int n) {
+        return (n*(n+1))/2;
+    }
+
+    public static void printChristmasTree(int height) {
+        for (int i = 1; i <= height; i++) {
+            String level = "";
+            for (int j = 0; j < (triangularNumber(height)-triangularNumber(i))/2; j++) {
+                if(i == 1){}
+                level += "  ";
+            }
+            if (triangularNumber(i) % 2 != 0 && triangularNumber(height) % 2 == 0) {
+                level += " ";
+            } else if (triangularNumber(i) % 2 == 0 && triangularNumber(height) % 2 != 0) {
+                level += " ";
+            }
+            for (int k = 0; k < triangularNumber(i); k++) {
+                level += "* ";
+            }
+            System.out.println(level);
+        }
+    }   
+}
